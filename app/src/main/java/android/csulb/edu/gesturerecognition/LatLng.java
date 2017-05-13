4ea7e39639cc619b140d2924ca1f8827ce69c3ea
aditi-p31 <aditi.patodia31@gmail.com>
2017-05-12 18:06:18 -0700

    Initial commit

---
+++ app/src/main/java/android/csulb/edu/gesturerecognition/LatLng.java
@@ -0,0 +1,14 @@
+package android.csulb.edu.gesturerecognition;
+
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+/**
+ * Created by Aditi on 5/10/2017.
+ */
+public class LatLng implements Serializable{
+    double latitude;
+    double longitude;
+    String addressString;
+}
